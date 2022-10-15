04f4b2b26cbefd4b09b5efecc8312efe86c01245
Alby7503 <alberto.vona24@gmail.com>
2022-10-16 00:04:27 +0200

    boom

--- WTree.py
+++ WTree.py
@@ -4,13 +4,13 @@ from typing import Union
 with open("words.txt", 'r', encoding="ascii") as words_file:
     words = words_file.read().splitlines()
 
-MAX_INDEXED_LETTERS = 2
+MAX_INDEXED_LETTERS = 4
 
 def index(word: str, position: int, current_index: dict) -> Union[dict, list]:
     """Index a word into the tree"""
     if position < len(word) and position < MAX_INDEXED_LETTERS:
-        correct_dictionary = {} if word[position] not in current_index else current_index[word[position]]
-        current_index[word[position]] = index(word, position + 1, correct_dictionary)
+        current_index[word[position]] = index(
+            word, position + 1, current_index.get(word[position], {}))
         return current_index
     if isinstance(current_index, list):
         current_index.append(word)
@@ -18,14 +18,15 @@ def index(word: str, position: int, current_index: dict) -> Union[dict, list]:
     return [word]
 
 
-def print_tree(current_index) -> None:
+def print_tree(current_index, previous: str = '') -> None:
     """Print an indexed tree formatted"""
+    print('\t' * len(previous), end = '')
     for branch in current_index:
         if type(current_index[branch]) == list:
             print(f"\t{current_index[branch]}", end='\n')
         else:
-            print(f"{branch}", end='\n\t')
-            print_tree(current_index[branch])
+            print(f"({previous}){branch}", end='\n')
+            print_tree(current_index[branch], previous + branch)
 
 
 def search_tree(word: str, source) -> Union[None, dict]:
@@ -45,11 +46,11 @@ def search_tree(word: str, source) -> Union[None, dict]:
 tree = {}
 for iword in words:
     index(iword, 0, tree)
-# print_tree(tree)
+#print_tree(tree)
 SEARCH_WORD = "re"
 search_result = search_tree(SEARCH_WORD, tree)
 if isinstance(search_result, dict):
-    print(SEARCH_WORD)
-    print_tree(search_result)
+    print(f"Search results for '{SEARCH_WORD}':")
+    print_tree(search_result, SEARCH_WORD)
 else:
     print(search_result)
